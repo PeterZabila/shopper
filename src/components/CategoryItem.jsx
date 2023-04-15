95c936f4b81f43ce7de7f955a47674426431b1c5
PZ <wikiwoo@PZ.local>
2023-04-16 01:49:42 +0300

    up to product

---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  flex: 1;
+  margin: 3px;
+  height: 70vh;
+`
+
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`
+const Title = styled.h1`
+    color: white;
+    margin-bottom: 20px;
+`
+const Info = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+`
+const Button = styled.button`
+    padding: 10px;
+    border: none;
+    color: gray;
+    font-weight: 600;
+    background-color: white;
+    cursor: pointer;
+`
+
+const CategoryItem = ({item}) => {
+  return (
+    <Container>
+      <Image src={item.img}/>
+      <Info>
+        <Title>{item.title}</Title>
+        <Button>SHOW NOW</Button>
+      </Info>
+    </Container>
+  )
+}
+
+export default CategoryItem
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import Navbar from '../components/Navbar';
 import Announcement from '../components/Announcement';
 import Slider from '../components/Slider';
+import Categories from '../components/Categories';
+import Products from '../components/Products';
+import NewsLetter from '../components/NewsLetter';
+import Footer from '../components/Footer';
 
 
 const Home = () => {
@@ -10,6 +14,10 @@ const Home = () => {
       <Announcement/>
       <Navbar/>
       <Slider/>
+      <Categories/>
+      <Products/>
+      <NewsLetter/>
+      <Footer/>
     </>
   )
 }
