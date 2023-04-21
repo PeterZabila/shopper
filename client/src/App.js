c2b382b4b06dc045fdb14a473699f5df6a441f41
PeterZabila <petrozabila@gmail.com>
2023-04-21 17:34:55 +0300

    added routes to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,44 @@
 import Home from './pages/Home';
+import Product from './pages/Product';
+import ProductsList from './pages/ProductsList';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Cart from './pages/Cart';
 import styled from 'styled-components';
+
+import { BrowserRouter as Router, Switch, Route, Redirect } from "react-router-dom"
 
 const Container = styled.div`
   height: 60px;
 `
 
 const App = () => {
+  const user = true;
   return (
-  <Container>
-    <Home/>
-  </Container>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/products">
+          <ProductsList/>
+        </Route>
+        <Route path="/products/:id">
+          <Product/>
+        </Route>
+        <Route path="/cart">
+          <Cart/>
+        </Route>
+        <Route path="/login">
+          {user ? <Redirect to="/"/> : <Login/>}
+        </Route>
+        <Route path="/register">
+          {user ? <Redirect to="/"/> : <Register/>}
+        </Route>
+      </Switch>
+
+
+  </Router>
   )
 };
 
